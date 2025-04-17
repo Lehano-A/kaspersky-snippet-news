e01ea724220d91c885198ce439e220727d775a61
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-17 12:08:45 +0300

    Обновляет конфиги prettier и vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
+import sassDts from 'vite-plugin-sass-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    sassDts(),
     react(),
     checker({
       typescript: { tsconfigPath: 'tsconfig.app.json' }
     })
   ],
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  },
   server: {
     port: 3000,
     strictPort: true,
