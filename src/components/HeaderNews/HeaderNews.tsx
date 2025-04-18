49c16062785d5daa76d28d8f17f54db018271b08
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-18 21:39:17 +0300

    Добавляет компонент HeaderNews

---
+++ src/components/HeaderNews/HeaderNews.tsx
@@ -0,0 +1,50 @@
+import Meta from './Meta/Meta'
+import HeaderToolbar from './Toolbar/Toolbar'
+import SourceDetails from './SourceDetails/SourceDetails'
+import Title from '../common/Title/Title'
+import { IData_SnippetNews } from '../../assets/data/dataNews.types'
+import { DataDuplicateNews } from '../../assets/data/dataDuplicateNews.types'
+
+export interface HeaderNewsProps {
+  miniMode?: boolean
+  duplicateNews?: DataDuplicateNews
+  dataNews: IData_SnippetNews
+}
+
+function HeaderNews({ miniMode, dataNews, duplicateNews }: HeaderNewsProps) {
+  const { AU, TI, DP, DOM, FAV, SENT, LANG, REACH, CNTR, CNTR_CODE, TRAFFIC } = dataNews
+
+  return (
+    <header className="header-news">
+      <div className="header-news__box-meta-toolbar">
+        <Meta
+          miniMode={miniMode}
+          datetime={duplicateNews ? duplicateNews['DP'] : DP}
+          reach={REACH}
+          traffic={TRAFFIC}
+        />
+
+        <HeaderToolbar
+          miniMode={miniMode}
+          SENT={SENT}
+        />
+      </div>
+
+      <Title variant="h2">{TI}</Title>
+
+      <div className={`${miniMode ? '' : 'mb-13'}`}>
+        <SourceDetails
+          miniMode={miniMode}
+          source={duplicateNews ? duplicateNews['DOM'] : DOM}
+          countryCode={CNTR_CODE}
+          country={CNTR}
+          lang={LANG}
+          favicon={duplicateNews ? duplicateNews['FAV'] : FAV}
+          author={AU}
+        />
+      </div>
+    </header>
+  )
+}
+
+export default HeaderNews
