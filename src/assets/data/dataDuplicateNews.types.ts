726d735a0a211174e0bda30140124d131c8ccac5
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-22 00:05:06 +0300

    Добавляет json-файл данных дубликатов, а также их файл типов

---
+++ src/assets/data/dataDuplicateNews.types.ts
@@ -0,0 +1,10 @@
+import { IData_SnippetNews } from './dataNews.types'
+
+export interface DataDuplicateNews {
+  DP: string
+  FAV: string
+  DOM: string
+  URL: string
+  AU: IData_SnippetNews['AU']
+  CNTR: string
+}
