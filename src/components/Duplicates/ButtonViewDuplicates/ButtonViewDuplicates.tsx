a842c80bf55e329df66fc5f9e8fa99377b0ebbac
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-21 22:29:08 +0300

    Добавляет компонент ButtonViewDuplicates

---
+++ src/components/Duplicates/ButtonViewDuplicates/ButtonViewDuplicates.tsx
@@ -0,0 +1,36 @@
+import withChangedAntdStyle from '../../../HOC/withChangedAntdStyle'
+import palette from '../../../scss/palette/_palette.module.scss'
+import { Button } from 'antd'
+import { useMemo } from 'react'
+import { DownOutlined } from '@ant-design/icons'
+
+interface ButtonViewDuplicatesProps {
+  handleClick: () => void
+}
+function ButtonViewDuplicates({ handleClick }: ButtonViewDuplicatesProps) {
+  const StyledButtonViewDuplicates = useMemo(
+    () =>
+      withChangedAntdStyle(Button, {
+        Button: {
+          defaultGhostBorderColor: palette.borderPrimary,
+          fontWeight: 200,
+          fontSize: '1.6rem',
+          letterSpacing: 3
+        }
+      }),
+    []
+  )
+  return (
+    <StyledButtonViewDuplicates
+      icon={<DownOutlined />}
+      block
+      ghost
+      size="large"
+      onClick={handleClick}
+    >
+      View Duplicates
+    </StyledButtonViewDuplicates>
+  )
+}
+
+export default ButtonViewDuplicates
