9c162ef4a0c85ba8164756e1b30de405023b1be9
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-22 01:03:10 +0300

    Добавляет глобальные стили

---
+++ src/scss/core/_global.module.scss.d.ts
@@ -0,0 +1,13 @@
+declare const classNames: {
+  readonly borderPrimary: 'borderPrimary'
+  readonly borderPrimaryAlpha30: 'borderPrimaryAlpha30'
+  readonly underlineLink: 'underlineLink'
+  readonly accentMain: 'accentMain'
+  readonly borderSecondary: 'borderSecondary'
+  readonly borderSecondaryAlpha30: 'borderSecondaryAlpha30'
+  readonly primaryMainText: 'primaryMainText'
+  readonly secondaryMainText: 'secondaryMainText'
+  readonly baseFontFamily: 'baseFontFamily'
+  readonly baseBoxBorderRadius: 'baseBoxBorderRadius'
+}
+export = classNames
