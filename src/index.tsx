0f897609e7dec7ff6aad558c2b3d17a384bacedf
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-22 01:08:12 +0300

    Правит index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConfigProvider } from 'antd'
 import './scss/index.scss'
+import '@ant-design/v5-patch-for-react-19'
 import App from './components/App/App'
+import themeAntd from './themeAntd'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider theme={themeAntd}>
+      <App />
+    </ConfigProvider>
   </StrictMode>
 )
