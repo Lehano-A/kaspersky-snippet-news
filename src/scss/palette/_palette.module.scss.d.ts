a381cecf8dbac03c03ac79e7cd88a305d08fb481
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-22 01:02:07 +0300

    Добавляет палитру

---
+++ src/scss/palette/_palette.module.scss.d.ts
@@ -0,0 +1,11 @@
+declare const classNames: {
+  readonly borderPrimary: 'borderPrimary'
+  readonly borderPrimaryAlpha30: 'borderPrimaryAlpha30'
+  readonly underlineLink: 'underlineLink'
+  readonly accentMain: 'accentMain'
+  readonly borderSecondary: 'borderSecondary'
+  readonly borderSecondaryAlpha30: 'borderSecondaryAlpha30'
+  readonly primaryMainText: 'primaryMainText'
+  readonly secondaryMainText: 'secondaryMainText'
+}
+export = classNames
