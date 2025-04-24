e5e3bd538faf7dd06ffb10e81a86240344aa416c
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-24 19:38:58 +0300

    Обновляет конфиг vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import sassDts from 'vite-plugin-sass-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kaspersky-snippet-news/",
   plugins: [
     sassDts(),
     react(),
